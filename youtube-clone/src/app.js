9004e30b4cdeb09dc51616c5f2539ad0968af94c
hakim <the9616@naver.com>
2022-01-05 23:16:33 +0900

    youtube-clone : youtube api fetch

--- youtube-clone/src/app.js
+++ youtube-clone/src/app.js
@@ -1,14 +1,25 @@
+import { useEffect, useState } from 'react';
 import './app.css';
-import Cards from './components/cards';
-import Navbar from './components/navbar';
+import VideoList from './components/video_list/video_list';
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <Cards />
-    </>
-  );
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    fetch(
+      'https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCMzcYX3E-Z2hcuf0gvNL3zKrF88Q6f1hQ',
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log('error', error));
+  }, []);
+  return <VideoList videos={videos} />;
 }
 
 export default App;
